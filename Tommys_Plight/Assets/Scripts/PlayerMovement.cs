05825d7c7e05749bd0fc9bda65208f45299f45b9
William Thomas <thomawil@ymail.com>
2019-01-03 02:18:20 -0500

    Added some more assets and started working on the intro

---
+++ Tommys_Plight/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Vector3.forward);
+    }
+}
